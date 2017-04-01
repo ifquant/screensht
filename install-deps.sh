7c856f98024b4e1a789afd048d41c79b18fddcd6
nvllvs <nvllvs@users.noreply.github.com>
2017-04-01 20:16:48 +0200

    Add install deps script

---
+++ install-deps.sh
@@ -0,0 +1,10 @@
+distro=$(cat /etc/*release*)
+distro=${distro,,}
+
+if [[ $distro == *"arch"* || $distro == *"manjaro"* ]]
+then
+	sudo pacman -S cmake base-devel libx11 libjpeg curl glib2 gdk-pixbuf2 libnotify
+elif [[ $distro == *"ubuntu"* || $distro == *"debian"* ]]
+then
+	sudo apt-get install cmake libx11-dev libjpeg-dev libcurl4-gnutls-dev libglib2.0-dev libgdk-pixbuf2.0-dev libnotify-dev
+fi
