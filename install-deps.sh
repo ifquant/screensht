515ff06b27e28340a52769b9adaf39c236b53168
nvllvs <nvllvs@users.noreply.github.com>
2017-04-10 12:25:52 +0200

    Add xclip to install-deps

--- install-deps.sh
+++ install-deps.sh
@@ -3,8 +3,8 @@ distro=${distro,,}
 
 if [[ $distro == *"arch"* || $distro == *"manjaro"* ]]
 then
-	sudo pacman -S cmake base-devel libx11 libjpeg curl glib2 gdk-pixbuf2 libnotify
+	sudo pacman -S cmake base-devel libx11 libjpeg curl glib2 gdk-pixbuf2 libnotify xclip
 elif [[ $distro == *"ubuntu"* || $distro == *"debian"* ]]
 then
-	sudo apt-get install cmake libx11-dev libjpeg-dev libcurl4-gnutls-dev libglib2.0-dev libgdk-pixbuf2.0-dev libnotify-dev
+	sudo apt-get install cmake libx11-dev libjpeg-dev libcurl4-gnutls-dev libglib2.0-dev libgdk-pixbuf2.0-dev libnotify-dev xclip
 fi
